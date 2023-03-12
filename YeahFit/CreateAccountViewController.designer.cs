20e732da82b146cd49ace54dbc9e408404a1d29c
nilswieloch <nils.wieloch@outlook.de>
2023-03-12 19:45:47 +0100

    IBOutlets

--- YeahFit/CreateAccountViewController.designer.cs
+++ YeahFit/CreateAccountViewController.designer.cs
@@ -12,9 +12,39 @@ namespace YeahFit
 	[Register ("CreateAccountViewController")]
 	partial class CreateAccountViewController
 	{
+		[Outlet]
+		UIKit.UIButton txtField_CreateUser { get; set; }
+
+		[Outlet]
+		UIKit.UITextField txtField_Password { get; set; }
+
+		[Outlet]
+		UIKit.UITextField txtField_PasswordRepeat { get; set; }
+
+		[Outlet]
+		UIKit.UITextField txtField_Username { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (txtField_Username != null) {
+				txtField_Username.Dispose ();
+				txtField_Username = null;
+			}
+
+			if (txtField_Password != null) {
+				txtField_Password.Dispose ();
+				txtField_Password = null;
+			}
+
+			if (txtField_PasswordRepeat != null) {
+				txtField_PasswordRepeat.Dispose ();
+				txtField_PasswordRepeat = null;
+			}
+
+			if (txtField_CreateUser != null) {
+				txtField_CreateUser.Dispose ();
+				txtField_CreateUser = null;
+			}
 		}
 	}
 }
--- YeahFit/LoginViewController.designer.cs
+++ YeahFit/LoginViewController.designer.cs
@@ -12,9 +12,39 @@ namespace YeahFit
 	[Register ("LoginViewController")]
 	partial class LoginViewController
 	{
+		[Outlet]
+		UIKit.UIButton btn_Login { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_LoginComment { get; set; }
+
+		[Outlet]
+		UIKit.UITextField txtField_Password { get; set; }
+
+		[Outlet]
+		UIKit.UITextField txtField_Username { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (lbl_LoginComment != null) {
+				lbl_LoginComment.Dispose ();
+				lbl_LoginComment = null;
+			}
+
+			if (txtField_Username != null) {
+				txtField_Username.Dispose ();
+				txtField_Username = null;
+			}
+
+			if (txtField_Password != null) {
+				txtField_Password.Dispose ();
+				txtField_Password = null;
+			}
+
+			if (btn_Login != null) {
+				btn_Login.Dispose ();
+				btn_Login = null;
+			}
 		}
 	}
 }
