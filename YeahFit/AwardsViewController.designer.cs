c929563bda672d0109660bbe13fa5e97e53ab101
nilswieloch <nils.wieloch@outlook.de>
2023-03-12 16:41:55 +0100

    IBOutlet Fehler gelöst uff

--- YeahFit/AwardsViewController.designer.cs
+++ YeahFit/AwardsViewController.designer.cs
@@ -13,6 +13,9 @@ namespace YeahFit
 	partial class AwardsViewController
 	{
 		[Outlet]
+		UIKit.UIImageView imageView_AdvancedMidWorkout { get; set; }
+
+		[Outlet]
 		UIKit.UIImageView imageView_AdvancedWorkout { get; set; }
 
 		[Outlet]
@@ -29,6 +32,9 @@ namespace YeahFit
 
 		[Outlet]
 		UIKit.UIImageView imageView_PerfectYear { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_AdvancedMidWorkoutCounter { get; set; }
 
 		[Outlet]
 		UIKit.UILabel lbl_AdvancedWorkout { get; set; }
@@ -56,44 +62,9 @@ namespace YeahFit
 		
 		void ReleaseDesignerOutlets ()
 		{
-			if (lbl_PerfectWeekCounter != null) {
-				lbl_PerfectWeekCounter.Dispose ();
-				lbl_PerfectWeekCounter = null;
-			}
-
-			if (imageView_PerfectWeek != null) {
-				imageView_PerfectWeek.Dispose ();
-				imageView_PerfectWeek = null;
-			}
-
-			if (lbl_PerfectMonthCounter != null) {
-				lbl_PerfectMonthCounter.Dispose ();
-				lbl_PerfectMonthCounter = null;
-			}
-
-			if (imageView_PerfectMonth != null) {
-				imageView_PerfectMonth.Dispose ();
-				imageView_PerfectMonth = null;
-			}
-
-			if (lbl_PerfectYeahrCounter != null) {
-				lbl_PerfectYeahrCounter.Dispose ();
-				lbl_PerfectYeahrCounter = null;
-			}
-
-			if (imageView_PerfectYear != null) {
-				imageView_PerfectYear.Dispose ();
-				imageView_PerfectYear = null;
-			}
-
-			if (lbl_PerfectYearCounter != null) {
-				lbl_PerfectYearCounter.Dispose ();
-				lbl_PerfectYearCounter = null;
-			}
-
-			if (lbl_ChallengeCounter != null) {
-				lbl_ChallengeCounter.Dispose ();
-				lbl_ChallengeCounter = null;
+			if (imageView_AdvancedWorkout != null) {
+				imageView_AdvancedWorkout.Dispose ();
+				imageView_AdvancedWorkout = null;
 			}
 
 			if (imageView_Challenge != null) {
@@ -101,9 +72,49 @@ namespace YeahFit
 				imageView_Challenge = null;
 			}
 
+			if (imageView_HardWorkout != null) {
+				imageView_HardWorkout.Dispose ();
+				imageView_HardWorkout = null;
+			}
+
+			if (imageView_PerfectMonth != null) {
+				imageView_PerfectMonth.Dispose ();
+				imageView_PerfectMonth = null;
+			}
+
+			if (imageView_PerfectWeek != null) {
+				imageView_PerfectWeek.Dispose ();
+				imageView_PerfectWeek = null;
+			}
+
+			if (imageView_PerfectYear != null) {
+				imageView_PerfectYear.Dispose ();
+				imageView_PerfectYear = null;
+			}
+
 			if (lbl_AdvancedWorkout != null) {
 				lbl_AdvancedWorkout.Dispose ();
 				lbl_AdvancedWorkout = null;
+			}
+
+			if (lbl_AdvancedWorkoutCounter != null) {
+				lbl_AdvancedWorkoutCounter.Dispose ();
+				lbl_AdvancedWorkoutCounter = null;
+			}
+
+			if (imageView_AdvancedMidWorkout != null) {
+				imageView_AdvancedMidWorkout.Dispose ();
+				imageView_AdvancedMidWorkout = null;
+			}
+
+			if (lbl_AdvancedMidWorkoutCounter != null) {
+				lbl_AdvancedMidWorkoutCounter.Dispose ();
+				lbl_AdvancedMidWorkoutCounter = null;
+			}
+
+			if (lbl_ChallengeCounter != null) {
+				lbl_ChallengeCounter.Dispose ();
+				lbl_ChallengeCounter = null;
 			}
 
 			if (lbl_HardWorkoutCounter != null) {
@@ -111,19 +122,24 @@ namespace YeahFit
 				lbl_HardWorkoutCounter = null;
 			}
 
-			if (imageView_AdvancedWorkout != null) {
-				imageView_AdvancedWorkout.Dispose ();
-				imageView_AdvancedWorkout = null;
+			if (lbl_PerfectMonthCounter != null) {
+				lbl_PerfectMonthCounter.Dispose ();
+				lbl_PerfectMonthCounter = null;
 			}
 
-			if (imageView_HardWorkout != null) {
-				imageView_HardWorkout.Dispose ();
-				imageView_HardWorkout = null;
+			if (lbl_PerfectWeekCounter != null) {
+				lbl_PerfectWeekCounter.Dispose ();
+				lbl_PerfectWeekCounter = null;
 			}
 
-			if (lbl_AdvancedWorkoutCounter != null) {
-				lbl_AdvancedWorkoutCounter.Dispose ();
-				lbl_AdvancedWorkoutCounter = null;
+			if (lbl_PerfectYeahrCounter != null) {
+				lbl_PerfectYeahrCounter.Dispose ();
+				lbl_PerfectYeahrCounter = null;
+			}
+
+			if (lbl_PerfectYearCounter != null) {
+				lbl_PerfectYearCounter.Dispose ();
+				lbl_PerfectYearCounter = null;
 			}
 		}
 	}
