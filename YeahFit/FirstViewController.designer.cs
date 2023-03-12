45aca5ab6cbca14251a781df6cc93641fb5521f3
nilswieloch <nils.wieloch@outlook.de>
2023-03-12 14:40:21 +0100

    IBOutlets

--- YeahFit/FirstViewController.designer.cs
+++ YeahFit/FirstViewController.designer.cs
@@ -13,6 +13,69 @@ namespace YeahFit
 	partial class FirstViewController
 	{
 		[Outlet]
+		UIKit.UIImageView imageView_FridayCircle { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView imageView_MondayCircle { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView imageView_TuesdayCircle { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView imageView_WednesdayCircle { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_Friday { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_FridayDate { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_Monday { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_MondayDate { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_Saturday { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView lbl_SaturdayCircle { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_SaturdayDate { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_Sunday { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView lbl_SundayCircle { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_SundayDate { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_Thursday { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView lbl_ThursdayCircle { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_ThursdayDate { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_Tuesday { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_TuesdayDate { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_Wednesday { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lbl_WednesdayDate { get; set; }
+
+		[Outlet]
 		UIKit.UICollectionView WeekCollectionView { get; set; }
 		
 		void ReleaseDesignerOutlets ()
@@ -20,6 +83,111 @@ namespace YeahFit
 			if (WeekCollectionView != null) {
 				WeekCollectionView.Dispose ();
 				WeekCollectionView = null;
+			}
+
+			if (imageView_MondayCircle != null) {
+				imageView_MondayCircle.Dispose ();
+				imageView_MondayCircle = null;
+			}
+
+			if (lbl_MondayDate != null) {
+				lbl_MondayDate.Dispose ();
+				lbl_MondayDate = null;
+			}
+
+			if (lbl_Monday != null) {
+				lbl_Monday.Dispose ();
+				lbl_Monday = null;
+			}
+
+			if (imageView_TuesdayCircle != null) {
+				imageView_TuesdayCircle.Dispose ();
+				imageView_TuesdayCircle = null;
+			}
+
+			if (lbl_TuesdayDate != null) {
+				lbl_TuesdayDate.Dispose ();
+				lbl_TuesdayDate = null;
+			}
+
+			if (lbl_Tuesday != null) {
+				lbl_Tuesday.Dispose ();
+				lbl_Tuesday = null;
+			}
+
+			if (lbl_WednesdayDate != null) {
+				lbl_WednesdayDate.Dispose ();
+				lbl_WednesdayDate = null;
+			}
+
+			if (imageView_WednesdayCircle != null) {
+				imageView_WednesdayCircle.Dispose ();
+				imageView_WednesdayCircle = null;
+			}
+
+			if (lbl_Wednesday != null) {
+				lbl_Wednesday.Dispose ();
+				lbl_Wednesday = null;
+			}
+
+			if (lbl_ThursdayDate != null) {
+				lbl_ThursdayDate.Dispose ();
+				lbl_ThursdayDate = null;
+			}
+
+			if (lbl_ThursdayCircle != null) {
+				lbl_ThursdayCircle.Dispose ();
+				lbl_ThursdayCircle = null;
+			}
+
+			if (lbl_Thursday != null) {
+				lbl_Thursday.Dispose ();
+				lbl_Thursday = null;
+			}
+
+			if (imageView_FridayCircle != null) {
+				imageView_FridayCircle.Dispose ();
+				imageView_FridayCircle = null;
+			}
+
+			if (lbl_FridayDate != null) {
+				lbl_FridayDate.Dispose ();
+				lbl_FridayDate = null;
+			}
+
+			if (lbl_Friday != null) {
+				lbl_Friday.Dispose ();
+				lbl_Friday = null;
+			}
+
+			if (lbl_SaturdayDate != null) {
+				lbl_SaturdayDate.Dispose ();
+				lbl_SaturdayDate = null;
+			}
+
+			if (lbl_Saturday != null) {
+				lbl_Saturday.Dispose ();
+				lbl_Saturday = null;
+			}
+
+			if (lbl_SaturdayCircle != null) {
+				lbl_SaturdayCircle.Dispose ();
+				lbl_SaturdayCircle = null;
+			}
+
+			if (lbl_SundayDate != null) {
+				lbl_SundayDate.Dispose ();
+				lbl_SundayDate = null;
+			}
+
+			if (lbl_SundayCircle != null) {
+				lbl_SundayCircle.Dispose ();
+				lbl_SundayCircle = null;
+			}
+
+			if (lbl_Sunday != null) {
+				lbl_Sunday.Dispose ();
+				lbl_Sunday = null;
 			}
 		}
 	}
