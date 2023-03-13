e626ab1c6506068c397168b5c041389eecb92605
nilswieloch <nils.wieloch@outlook.de>
2023-03-13 18:13:58 +0100

    Push

---
+++ YeahFit/SetWeek.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Drawing;
+using MySql.Data.MySqlClient;
+
+namespace YeahFit
+{
+	public class SetWeek
+	{
+        public static MySqlConnection con;
+
+        static int weekID = 0;
+
+        public SetWeek()
+		{
+		}
+
+        public static void SelectSetWeek(bool internalMonday, bool internalTuesday, bool internalWednesday, bool internalThursday, bool internalFriday, bool internalSaturday, bool internalSunday)
+        {
+            if (LoginViewController.loggedin == true)
+            {
+                weekID = 0;
+
+                // Open a connection to the YeahFit database on the local server with the root user and empty password.
+                con = new MySqlConnection(@"Server=localhost;Database=YeahFit;User Id=root;Password=; CharSet = utf8");
+                con.Open();
+                using (MySqlCommand getWeek = new MySqlCommand($"SELECT * FROM `Woche` " +
+                    $"WHERE Montag='{internalMonday}' " +
+                    $"AND Dienstag='{internalTuesday}' " +
+                    $"AND Mittwoch='{internalWednesday}' " +
+                    $"AND Donnerstag='{internalThursday}' " +
+                    $"AND Freitag='{internalFriday}' " +
+                $"AND Samstag='{internalSaturday}' " +
+                    $"AND Sonntag='{internalSunday}' ", con))
+                {
+                    using (MySqlDataReader reader = getWeek.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            weekID = Convert.ToInt32(reader["WochenID"]);
+                        }
+                    }
+                }
+                if (weekID == 0)
+                {
+                    MySqlCommand insert = new MySqlCommand($"INSERT INTO Woche (Montag, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag) " +
+                        $"VALUES ('{internalMonday}', '{internalTuesday}', '{internalWednesday}', '{internalThursday}', '{internalFriday}', '{internalSaturday}', '{internalSunday}');", con);
+                    insert.ExecuteNonQuery();
+                    using (MySqlCommand getNewWeek = new MySqlCommand($"SELECT WochenID FROM `Woche`, `Benutzer_Woche` " +
+                        $"WHERE Woche.WochenID = Benutzer_Woche.WochenID " +
+                        $"AND Benutzer_Woche.BenutzerID = {LoginViewController.userID}", con))
+                    {
+                        using (MySqlDataReader reader2 = getNewWeek.ExecuteReader())
+                        {
+
+                            while (reader2.Read())
+                            {
+                                weekID = Convert.ToInt32(reader2["WochenID"]);
+                            }
+                        }
+                    }
+                    InitializeWeek.wochenid = weekID;
+                }
+                else
+                {
+                    MySqlCommand insert2 = new MySqlCommand($"INSERT INTO Benutzer_Woche (BenutzerID, WochenID) " +
+                        $"VALUES ('{LoginViewController.userID}', '{weekID}';", con);
+                    insert2.ExecuteNonQuery();
+                }
+                con.Close();
+            }
+        }
+	}
+}
+
--- YeahFit/WeekSettingsViewController.cs
+++ YeahFit/WeekSettingsViewController.cs
@@ -3,12 +3,17 @@
 using System;
 
 using Foundation;
+using MySql.Data.MySqlClient;
 using UIKit;
 
 namespace YeahFit
 {
     public partial class WeekSettingsViewController : UIViewController
     {
+        //public static MySqlConnection con;
+
+        //int weekID = 0;
+
         public static FirstViewController firstViewController;
 
         bool internalMonday = false;
@@ -131,8 +136,68 @@ namespace YeahFit
             };
             btn_SaveWeek.TouchUpInside += (sender, e) =>
             {
-                if (LoginViewController.loggedin == true)
+                SetWeek.SelectSetWeek(internalMonday, internalTuesday, internalWednesday, internalTuesday, internalFriday, internalSaturday, internalSunday);
+
+                /*if (LoginViewController.loggedin == true)
                 {
+                    weekID = 0;
+
+                    // Open a connection to the YeahFit database on the local server with the root user and empty password.
+                    con = new MySqlConnection(@"Server=localhost;Database=YeahFit;User Id=root;Password=; CharSet = utf8");
+                    con.Open();
+
+                    using (MySqlCommand getWeek = new MySqlCommand($"SELECT * FROM `Woche` " +
+                        $"WHERE Montag='{internalMonday}' " +
+                        $"AND Dienstag='{internalTuesday}' " +
+                        $"AND Mittwoch='{internalWednesday}' " +
+                        $"AND Donnerstag='{internalThursday}' " +
+                        $"AND Freitag='{internalFriday}' " +
+                        $"AND Samstag='{internalSaturday}' " +
+                        $"AND Sonntag='{internalSunday}' ", con))
+                    {
+                        using (MySqlDataReader reader = getWeek.ExecuteReader())
+                        {
+
+                            while (reader.Read())
+                            {
+                                weekID = Convert.ToInt32(reader["WochenID"]);
+                            }
+                        }
+                    }
+
+                    if (weekID == 0)
+                    {
+                        MySqlCommand insert = new MySqlCommand($"INSERT INTO Woche (Montag, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag) " +
+                            $"VALUES ('{internalMonday}', '{internalTuesday}', '{internalWednesday}', '{internalThursday}', '{internalFriday}', '{internalSaturday}', '{internalSunday}');", con);
+                        insert.ExecuteNonQuery();
+
+                        using (MySqlCommand getNewWeek = new MySqlCommand($"SELECT WochenID FROM `Woche`, `Benutzer_Woche` " +
+                            $"WHERE Woche.WochenID = Benutzer_Woche.WochenID " +
+                            $"AND Benutzer_Woche.BenutzerID = {LoginViewController.userID}", con))
+                        {
+                            using (MySqlDataReader reader2 = getNewWeek.ExecuteReader())
+                            {
+
+                                while (reader2.Read())
+                                {
+                                    weekID = Convert.ToInt32(reader2["WochenID"]);
+                                }
+                            }
+                        }
+                        InitializeWeek.wochenid = weekID;
+                    }
+                    else
+                    {
+                        MySqlCommand insert2 = new MySqlCommand($"INSERT INTO Benutzer_Woche (BenutzerID, WochenID) " +
+                            $"VALUES ('{LoginViewController.userID}', '{weekID}';", con);
+                        insert2.ExecuteNonQuery();
+                    }
+                    con.Close();
+                */
+
+
+
+
 
                     this.DismissViewController(true, () => { FirstViewController.Refresh(firstViewController); });
                 }
